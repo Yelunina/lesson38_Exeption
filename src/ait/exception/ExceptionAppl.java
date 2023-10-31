341c8f381d62cfbf24582c55864a4287304306e9
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-10-31 20:09:50 +0100

    code

--- src/ait/exception/ExceptionAppl.java
+++ src/ait/exception/ExceptionAppl.java
@@ -1,33 +1,38 @@
 package ait.exception;
 
 public class ExceptionAppl {
-    public static void main(String[] args) {
-        int a = 0;
-        int b = 0;
+    public static void main (String[] args) {
+        int a = 3;
+        int b = 12;
         try {
             int x = solution(a, b);
             System.out.println("Solution = " + x);
-        } catch (ArithmeticException e) {
+
+        } catch (NoSolutionException e) {
 //            e.printStackTrace();
-            System.out.println("Solution any number");
-            return;
-        } catch (IllegalArgumentException e) {
+//            System.out.println(e.getMessage());
             System.out.println("No solution");
-            return;
-        } finally {
+
+        } catch (SolutionAnyNumberException e) {
+//            System.out.println(e.getMessage());
+            System.out.println("Solution any number");
+
+        }  finally {
             System.out.println("Bye, bye");
         }
-        System.out.println("Math is great!");
+        System.out.println("Math is great");
+
     }
 
-    public static int solution(int a, int b) {
+    public static int solution(int a, int b) throws NoSolutionException {
+        int res;
         if (a == 0 && b != 0) {
-            throw new IllegalArgumentException();
+            throw new NoSolutionException();
         }
         if (a == 0 && b == 0) {
-            throw new ArithmeticException();
+            throw new SolutionAnyNumberException();
         }
-        int res = b / a;
+        res = b / a;
         return res;
     }
 }
